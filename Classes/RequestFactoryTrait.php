3588a94343eb4c6af3c75d143d96ec3783d36780
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2026-07-27 09:55:07 +0200

    TASK: Use `php-cs-fixer` to optimise imports and correct spacing at begin of PHP files
    as required by PSR12
    
    config
    
    ```
    'blank_line_after_opening_tag' => true,
    'blank_lines_before_namespace' => true,
    'no_unused_imports' => true,
    'ordered_imports' => [
        'sort_algorithm' => 'alpha',
    ]
    ```

--- Classes/RequestFactoryTrait.php
+++ Classes/RequestFactoryTrait.php
@@ -1,10 +1,10 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Neos\Http\Factories;
 
 use GuzzleHttp\Psr7\Request;
-use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 
 /**
